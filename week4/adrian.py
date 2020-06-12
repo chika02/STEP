34e36250d313d86ef23a995d556526ed7192b952
chikaokuda <okudachika@icloud.com>
2020-06-12 16:32:24 +0900

    implemented bfr

--- week4/adrian.py
+++ week4/adrian.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 def openfiles():
     with open('nicknames.txt') as f1:
         names = {}
@@ -5,32 +7,51 @@ def openfiles():
             id_, name = s.strip().split()
             id_ = int(id_)
             names[name]=id_
+    n = len(names)
     with open('links.txt') as f2:
         follow = [ [] for _ in range (n)]
         for s in f2.readlines():
             s1, s2 = s.strip().split()
-            follow[int(s1)].append(s2)
-        print(follow)
-    return names, follow
+            follow[int(s1)].append(int(s2))
+    return names, follow, n
 
-def search_ways(id1,id2):
 
-    return True
+def search_ways(id1, id2, follow, n):
 
-def main(from_name, to_name, names, follow):
+    if id1 == id2:
+        return True
+    flg = ['UNDONE' for _ in range(n)]
+    q = deque()
+    q.append(id1)
+    flg[id1] = 'DOING'
+    while len(q) != 0:
+        person = q.popleft()
+        for sb in follow[person]:
+            if flg[sb] == 'UNDONE':
+                #print(sb, end = " ")
+                if sb == id2:
+                    return True
+                else:
+                    q.append(sb)
+                    flg[sb] = 'DOING'
+        flg[person] = 'DONE'
+
+    return False
+
+def main(from_name, to_name, names, follow, n):
 
     if from_name in names:
         id1 = names[from_name]
     else:
-        print("cannot find %s" % from_name)
+        print("%s does not exist" % from_name)
         return
     if to_name in names:
         id2 = names[to_name]
     else:
-        print("cannot find %s" % from_name)
+        print("%s does not exist" % to_name)
         return
 
-    ans  = search_ways(id1, id2)
+    ans  = search_ways(id1, id2, follow, n)
     if ans == 1:
         print("%s to %s: found!" % (from_name, to_name))
     else:
@@ -38,22 +59,22 @@ def main(from_name, to_name, names, follow):
     return
 
 
-def runTest(names, follow):
+def runTest(names, follow, n):
 
-    main("adrian", "judith", names, follow)
-    main("adrian", "cynthia", names, follow)
-    main("cynthia", "cynthia", names, follow)
-    main("adrian", "abracadabra", names, follow)
-    main("xxxx", "adrian", names, follow)
+    main("adrian", "judith", names, follow, n)
+    main("adrian", "cynthia", names, follow, n)
+    main("cynthia", "cynthia", names, follow, n)
+    main("adrian", "abracadabra", names, follow, n)
+    main("xxxx", "adrian", names, follow, n)
 
 
 if __name__ == "__main__":
 
-    names, follow = openfiles()
-    runTest(names, follow)
+    names, follow, n = openfiles()
+    runTest(names, follow, n)
     while(1):
         print("> from:", end="")
-        name1 = input()
+        name1 = input().strip()
         print("> to:", end="")
-        name2 = input()
-        main(name1, name2, names, follow)
+        name2 = input().strip()
+        main(name1, name2, names, follow, n)
