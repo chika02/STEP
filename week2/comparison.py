83a40b7f7f9bfa4597b43221f77d9a074f879f14
chikaokuda <okudachika@icloud.com>
2020-06-19 10:02:53 +0900

    comparison.py

---
+++ week2/comparison.py
@@ -0,0 +1,45 @@
+import sys
+import numpy as np
+import time
+
+def array_generator(n):
+    array = np.random.rand(n,n)
+    return array
+
+
+def product_manual(A,B,n):
+    C = [[0 for _ in range(n)] for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            C[i][j] = sum(A[i][k]*B[k][j] for k in range(n))
+    return C
+
+
+def product_np(A,B):
+    product = np.dot(A,B)
+    return product
+
+
+if __name__ == "__main__":
+
+    args = sys.argv
+    if len(args) != 2 or not args[1].isdigit():
+        print("invalid input",file = sys.stderr)
+        sys.exit(1)
+    n = int(args[1])
+
+    A = array_generator(n)
+    B = array_generator(n)
+    t1 = time.time()
+    C = product_manual(A,B,n)
+    t2 = time.time()
+    D = product_np(A,B)
+    t3 = time.time()
+
+    path1 = "time_manual.csv"
+    path2 = "time_np.csv"
+    with open (path1, mode="a") as f:
+        f.write("%d  %lf\n" %(n, (t2-t1)*1000))
+    with open (path2, mode="a") as f:
+        f.write("%d  %lf\n" %(n, (t3-t2)*1000))
+
