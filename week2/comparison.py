f48ca62eef8c93e2fead57fac3d3a1c1a2bb8646
chikaokuda <okudachika@icloud.com>
2020-06-19 16:33:54 +0900

    iterate for n

--- week2/comparison.py
+++ week2/comparison.py
@@ -19,30 +19,41 @@ def product_np(A,B):
     product = np.dot(A,B)
     return product
 
-
-if __name__ == "__main__":
-
-    args = sys.argv
-    if len(args) != 2:
-        print("usage: python comparison.py <number>",file = sys.stderr)
-        sys.exit(1)
-    if not args[1].isdigit():
-        print("invalid character detected",file = sys.stderr)
-        sys.exit(1)
-    n = int(args[1])
+def measure_time_py(n):
 
     A = array_generator(n)
     B = array_generator(n)
     t1 = time.time()
     C = product_py(A,B,n)
     t2 = time.time()
-    D = product_np(A,B)
-    t3 = time.time()
+
+    return (t2-t1)*10**6
+
+def measure_time_np(n):
+
+    A = array_generator(n)
+    B = array_generator(n)
+    t1 = time.time()
+    C = product_np(A,B)
+    t2 = time.time()
+
+    return (t2-t1)*10**6
+
+if __name__ == "__main__":
 
     path1 = "time_py.csv"
     path2 = "time_np.csv"
-    with open (path1, mode="a") as f:
-        f.write("%d  %lf\n" %(n, (t2-t1)*10**6))
-    with open (path2, mode="a") as f:
-        f.write("%d  %lf\n" %(n, (t3-t2)*10**6))
-
+    max_n = 100
+    m = 10
+    ave = 0
+    with open (path1, mode="w") as f:
+        for n in range(max_n):
+            for j in range(m):
+                ave += measure_time_py(n)*1.0/m
+            f.write("%d  %lf\n" %(n, ave))
+    ave = 0
+    with open (path2, mode="w") as f:
+        for n in range(max_n):
+            for j in range(m):
+                ave += measure_time_np(n)*1.0/m
+            f.write("%d  %lf\n" %(n, ave))
